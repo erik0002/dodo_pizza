97b285354467b01a69f9b0897720fe79780d23aa
Yeliazar <erik.ruzhakov@mail.ru>
2021-08-30 21:40:13 +0300

    updated categories and header

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+// class Categories extends React.Component {
+//
+//     state = {
+//         activeItem: 3
+//     }
+//
+//     onSelectItem = index => {
+//         this.setState({
+//             activeItem: index
+//         })
+//     }
+//
+//     render () {
+//         const {items, onClickItem} = this.props;
+//         const categories = items.map((item, i) => <li
+//             className={this.state.activeItem === i ? 'active' : ''}
+//             onClick={() => this.onSelectItem(i)}
+//             key={`${item}_${i}`}>{item} </li>)
+//         return (
+//             <div className="categories">
+//                 <ul>
+//                     <li>Все</li>
+//                     {categories}
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default Categories;
+export default function Categories({items}) {
+
+    const [activeItem, setActiveItem] = React.useState(null);
+
+    const onSelectItem = (index) => {
+        setActiveItem(index);
+    }
+
+    const categories = items && items.map((item, i) => (
+        <li
+            className={activeItem === i ? 'active' : ''}
+            onClick={() => onSelectItem(i)}
+            key={`${item}_${i}`}>{item}
+        </li>
+    ))
+
+    return (
+        <div className="categories">
+            <ul>
+                <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
+                {categories}
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+
+export { default as Button } from './button/button';
+export { default as Categories } from './categories/Categories';
+export { default as Header } from './header/header';
+
