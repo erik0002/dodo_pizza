9080565295a9c6d557cfbf9de8ba229ae18089f3
Yeliazar <erik.ruzhakov@mail.ru>
2021-08-31 10:39:19 +0300

    updated popup

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@
 export { default as Button } from './button/button';
 export { default as Categories } from './categories/Categories';
 export { default as Header } from './header/header';
+export {default as SortPopUp } from './SortPopUp/SortPopUp';
 
