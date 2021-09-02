9169e57914031c7d0403b641e657fb5f87411ab2
Yeliazar <erik.ruzhakov@mail.ru>
2021-09-02 19:39:53 +0300

    updated redux-thunk, updated get-request, add in creator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React  from "react";
 import './App.css';
-import axios from "axios";
-//import logoSvg from './asserts/img/pizza-logo.svg';
 import {Header} from "./components";
 import {Main, Basket} from "./pages";
 import {Route} from "react-router-dom";
 import {useDispatch} from "react-redux";
 // import {connect} from 'react-redux';
-import {setPizzas} from "./redux/actions/pizzas";
+import {fetchPizzas} from "./redux/actions/pizzas";
 
 
 function App(){
-
     const dispatch = useDispatch();
 
 
     React.useEffect(() => {
-        axios.get('http://localhost:3001/pizzas?_order=asc&_sort=price').then(({ data }) => {
-            dispatch(setPizzas(data));
-        })
+        dispatch(fetchPizzas());
     }, []);
 
     return (
